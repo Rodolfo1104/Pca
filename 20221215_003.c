1ed0eae73235114490521650f3f52660631e4c07
Rodolfo1104 <119976015+Rodolfo1104@users.noreply.github.com>
2022-12-15 18:58:11 -0400

    Create 20221215_003.c

---
+++ 20221215_003.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+
+{
+   float v1, v2;
+
+  printf("Digite o valor em m: ");
+  scanf("%f", &v1);
+
+  v2 = v1 * 100;
+  printf("\n Valor em cm = %f", v2);
+  return 0;
+}
