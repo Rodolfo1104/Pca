661d14bb0daf1dcd034578db063855aef1ffe146
Rodolfo1104 <119976015+Rodolfo1104@users.noreply.github.com>
2023-01-16 14:35:33 -0400

    Create Prova1questao1.c

---
+++ Prova1questao1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void input()
+{
+    float lowest, highest, n, average = 0, sum = 0;
+
+    for (int i = 1; i > 0; i++)
+    {
+        printf("Input the student's grade: ");
+        scanf("%f", &n);
+
+        if (n > 0)
+        {
+            sum += n;
+            average = sum / i;
+        }
+
+        if (n < 0)
+        {
+            printf("Stopping program...\n");
+            break;
+        }
+
+        if (i == 1)
+        {
+            highest = lowest = n;
+        } else if (n > highest)
+        {
+            highest = n;
+        } else if (n < lowest)
+        {
+            lowest = n;
+        }
+    }
+
+    printf("Highest grade: [%.2f]\n", highest);
+    printf("Lowest grade: [%.2f]\n", lowest);
+    printf("Average grade: [%.2f]\n", average);
+}
+
+int main(int argc, char const *argv[])
+{
+    input();
+    return 0;
+}
