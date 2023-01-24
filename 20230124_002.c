e6439654a14d74e07882264f31d238256bcf71dc
Rodolfo1104 <119976015+Rodolfo1104@users.noreply.github.com>
2023-01-24 17:40:00 -0400

    Create 20230124_002.c

---
+++ 20230124_002.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int list [100]; 
+int i, qt;
+
+void function ()
+{
+    printf("How long is the sequence? ");
+    scanf("%i", &qt);
+
+    for (i = 1; i <= qt; i++)
+    {
+        printf("\nType a number. ");
+        scanf("%i", &list[i]);
+    }
+
+    printf("\n");
+
+    for (i = qt; i <= qt && i > 0; i--)
+    {
+        printf("[%i]", list[i]);
+    }
+
+}
+
+int main(int argc, char const *argv[])
+{
+    function();
+    return 0;
+}
