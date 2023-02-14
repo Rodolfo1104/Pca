88cd486344804fb128049f34917341d950bc8534
Rodolfo1104 <119976015+Rodolfo1104@users.noreply.github.com>
2023-02-14 13:41:28 -0400

    Update 20230126_004.c

--- 20230126_004.c
+++ 20230126_004.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Leia uma matriz 3 x 3. Leia também um valor X. O programa deverá fazer uma busca desse valor na matriz e, ao final escrever a localização (linha e coluna) ou uma mensagem de “não encontrado”.
-
-
 int main(){
 
 int m[3][3], i, j, x;
